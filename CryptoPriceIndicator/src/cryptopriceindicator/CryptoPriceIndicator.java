7f8164501e9a622bd5e37bb2cb349d6149a942c8
Anuk Silva <vwv9667@autuni.ac.nz>
2021-11-16 01:39:51 +1300

    GUI Frame Created
    GUI Frame Created for the App

--- CryptoPriceIndicator/src/cryptopriceindicator/CryptoPriceIndicator.java
+++ CryptoPriceIndicator/src/cryptopriceindicator/CryptoPriceIndicator.java
@@ -5,17 +5,41 @@
  */
 package cryptopriceindicator;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import org.jsoup.Jsoup;
+
 /**
  *
  * @author Anuk
  */
-public class CryptoPriceIndicator {
+public class CryptoPriceIndicator extends JFrame implements ActionListener{
+    
+    
+    public CryptoPriceIndicator(){
+        
+        this.setResizable(false);
+        this.setSize(1200, 700);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.lightGray);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Crypto Prices");
+        
+        this.setVisible(true);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new CryptoPriceIndicator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
     
 }
