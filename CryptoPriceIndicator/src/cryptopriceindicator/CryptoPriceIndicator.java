1abc3d72d2cd0722975e76fed3279915ffa27bb0
Anuk Silva <vwv9667@autuni.ac.nz>
2021-11-19 21:17:16 +1300

    Added 3rd Coin Information
    Added the 3rd Highest Market Cap Coin information regarding the coin's name, ticker, price, market cap and current circulating supply

--- CryptoPriceIndicator/src/cryptopriceindicator/CryptoPriceIndicator.java
+++ CryptoPriceIndicator/src/cryptopriceindicator/CryptoPriceIndicator.java
@@ -62,6 +62,20 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
     public static String secondCoinTempMC;
     public static String secondCoinTempCS;
     
+    private JLabel thirdCoinRank;
+    private JLabel thirdCoin;
+    private JLabel thirdCoinPrice;
+    private JLabel thirdCoinMC;
+    private JLabel thirdCoinCS;
+  
+    public static String thirdCoinTicker;
+    public static String thirdCoinTickerTrimmed;
+    
+    public static String thirdCoinTempPrice;
+    public static String thirdCoinTempRank;
+    public static String thirdCoinTempMC;
+    public static String thirdCoinTempCS;
+    
     public CryptoPriceIndicator(){
         
         this.setResizable(false);
@@ -128,7 +142,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         firstCoinRank = new JLabel();
         firstCoinRank.setText(firstCoinTempRank);
         firstCoinRank.setForeground(Color.white);
-        firstCoinRank.setFont(new Font("Arial", Font.BOLD, 30));
+        firstCoinRank.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 0;
         c.gridy = 1;
         c.insets = new Insets(20, -50, 40, 160);
@@ -138,7 +152,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         firstCoin = new JLabel();
         firstCoin.setText(firstCoinTickerTrimmed + ":");
         firstCoin.setForeground(Color.white);
-        firstCoin.setFont(new Font("Arial", Font.BOLD, 30));
+        firstCoin.setFont(new Font("Arial", Font.BOLD, 25));
         c.anchor = GridBagConstraints.WEST;
         c.gridx = 1;
         c.gridy = 1;
@@ -149,7 +163,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         firstCoinPrice = new JLabel();
         firstCoinPrice.setText(firstCoinTempPrice);
         firstCoinPrice.setForeground(Color.white);
-        firstCoinPrice.setFont(new Font("Arial", Font.BOLD, 30));
+        firstCoinPrice.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 2;
         c.gridy = 1;
         c.insets = new Insets(20, -120, 40, 150);
@@ -159,7 +173,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         firstCoinMC = new JLabel();
         firstCoinMC.setText(firstCoinTempMC);
         firstCoinMC.setForeground(Color.white);
-        firstCoinMC.setFont(new Font("Arial", Font.BOLD, 30));
+        firstCoinMC.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 3;
         c.gridy = 1;
         c.insets = new Insets(20, -50, 40, 80);        
@@ -170,7 +184,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         firstCoinCS = new JLabel();
         firstCoinCS.setText(firstCoinTempCS);
         firstCoinCS.setForeground(Color.white);
-        firstCoinCS.setFont(new Font("Arial", Font.BOLD, 30));
+        firstCoinCS.setFont(new Font("Arial", Font.BOLD, 25));
         c.anchor = GridBagConstraints.EAST;
         c.gridx = 4;
         c.gridy = 1;
@@ -183,7 +197,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         secondCoinRank = new JLabel();
         secondCoinRank.setText(secondCoinTempRank);
         secondCoinRank.setForeground(Color.white);
-        secondCoinRank.setFont(new Font("Arial", Font.BOLD, 30));
+        secondCoinRank.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 0;
         c.gridy = 2;
         c.insets = new Insets(20, -50, 40, 160);
@@ -193,17 +207,19 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         secondCoin = new JLabel();
         secondCoin.setText(secondCoinTickerTrimmed + ":");
         secondCoin.setForeground(Color.white);
-        secondCoin.setFont(new Font("Arial", Font.BOLD, 30));
+        secondCoin.setFont(new Font("Arial", Font.BOLD, 25));
+        c.anchor = GridBagConstraints.WEST;
         c.gridx = 1;
         c.gridy = 2;
         c.insets = new Insets(20, -120, 40, 150);
 
         coinsPanel.add(secondCoin, c);
+        c.anchor = GridBagConstraints.EAST;
 
         secondCoinPrice = new JLabel();
         secondCoinPrice.setText(secondCoinTempPrice);
         secondCoinPrice.setForeground(Color.white);
-        secondCoinPrice.setFont(new Font("Arial", Font.BOLD, 30));
+        secondCoinPrice.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 2;
         c.gridy = 2;
         c.insets = new Insets(20, -120, 40, 150);
@@ -213,7 +229,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         secondCoinMC = new JLabel();
         secondCoinMC.setText(secondCoinTempMC);
         secondCoinMC.setForeground(Color.white);
-        secondCoinMC.setFont(new Font("Arial", Font.BOLD, 30));
+        secondCoinMC.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 3;
         c.gridy = 2;
         c.insets = new Insets(20, -50, 40, 80);
@@ -223,12 +239,64 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         secondCoinCS = new JLabel();
         secondCoinCS.setText(secondCoinTempCS);
         secondCoinCS.setForeground(Color.white);
-        secondCoinCS.setFont(new Font("Arial", Font.BOLD, 30));
+        secondCoinCS.setFont(new Font("Arial", Font.BOLD, 25));
         c.gridx = 4;
         c.gridy = 2;
         c.insets = new Insets(20, -50, 40, 0);
         
         coinsPanel.add(secondCoinCS, c);
+        
+        //3rd coin below
+        
+        thirdCoinRank = new JLabel();
+        thirdCoinRank.setText(thirdCoinTempRank);
+        thirdCoinRank.setForeground(Color.white);
+        thirdCoinRank.setFont(new Font("Arial", Font.BOLD, 25));
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(20, -50, 40, 160);
+
+        coinsPanel.add(thirdCoinRank, c);
+        
+        thirdCoin = new JLabel();
+        thirdCoin.setText(thirdCoinTickerTrimmed + ":");
+        thirdCoin.setForeground(Color.white);
+        thirdCoin.setFont(new Font("Arial", Font.BOLD, 25));
+        c.gridx = 1;
+        c.gridy = 3;
+        c.insets = new Insets(20, -120, 40, 150);
+
+        coinsPanel.add(thirdCoin, c);
+
+        thirdCoinPrice = new JLabel();
+        thirdCoinPrice.setText(thirdCoinTempPrice);
+        thirdCoinPrice.setForeground(Color.white);
+        thirdCoinPrice.setFont(new Font("Arial", Font.BOLD, 25));
+        c.gridx = 2;
+        c.gridy = 3;
+        c.insets = new Insets(20, -120, 40, 150);
+        
+        coinsPanel.add(thirdCoinPrice, c);
+        
+        thirdCoinMC = new JLabel();
+        thirdCoinMC.setText(thirdCoinTempMC);
+        thirdCoinMC.setForeground(Color.white);
+        thirdCoinMC.setFont(new Font("Arial", Font.BOLD, 25));
+        c.gridx = 3;
+        c.gridy = 3;
+        c.insets = new Insets(20, -50, 40, 80);
+
+        coinsPanel.add(thirdCoinMC, c);
+        
+        thirdCoinCS = new JLabel();
+        thirdCoinCS.setText(thirdCoinTempCS);
+        thirdCoinCS.setForeground(Color.white);
+        thirdCoinCS.setFont(new Font("Arial", Font.BOLD, 25));
+        c.gridx = 4;
+        c.gridy = 3;
+        c.insets = new Insets(20, -50, 40, 0);
+        
+        coinsPanel.add(thirdCoinCS, c);
         
         this.setVisible(true);
     }
@@ -316,6 +384,48 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
             ex.printStackTrace();
         }
     }
+    
+    static void webScrape3rdCoin() {
+        int count = 0;
+        final String url = "https://coinmarketcap.com/";
+
+        try {
+            final Document document = Jsoup.connect(url).userAgent("Mozilla/17.0").timeout(10000).get();
+
+            for (Element row : document.select(
+                    "table.h7vnx2-2.czTsgW.cmc-table tr")) {
+                if (row.select("td:nth-of-type(3)").text().equals("")) {
+                    continue;
+                } else {
+                    String num1ToRemove = "3";
+                    thirdCoinTicker = row.select("td:nth-of-type(3)").text();
+                    thirdCoinTickerTrimmed = thirdCoinTicker.substring(0, thirdCoinTicker.length() - 3);
+                    System.out.println(thirdCoinTicker.substring(0, thirdCoinTicker.length() - 3));
+                    if (thirdCoinTickerTrimmed.contains(num1ToRemove)) {
+                        thirdCoinTickerTrimmed = thirdCoinTickerTrimmed.replaceAll(num1ToRemove, "");
+                    }
+                    thirdCoinTempPrice = row.select("td:nth-of-type(4)").text();
+                    System.out.println(thirdCoinTempPrice);
+                    
+                    thirdCoinTempRank = row.select("td:nth-of-type(2)").text();
+                    System.out.println(thirdCoinTempRank);
+                    
+                    thirdCoinTempMC = row.select(".ieFnWP.sc-1ow4cwt-1").text();
+                    System.out.println(thirdCoinTempMC);
+                    
+                    thirdCoinTempCS = row.select("td:nth-of-type(9)").text();
+                    System.out.println(thirdCoinTempCS);
+                    
+                }
+                count++;
+                if (count == 3) {
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -324,6 +434,7 @@ public class CryptoPriceIndicator extends JFrame implements ActionListener{
         // TODO code application logic here
         webScrape1stCoin();
         webScrape2ndCoin();
+        webScrape3rdCoin();
         new CryptoPriceIndicator();
     }
 
